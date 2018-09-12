30b75c3214fb4d1d866a30f50a75747740bbde20
Jon <mail@jshimwell.com>
2018-09-12 15:01:21 +0100

    fixed imports with r

--- breeder_blanket_model_maker/detailed_module.py
+++ breeder_blanket_model_maker/detailed_module.py
@@ -5,7 +5,6 @@ import sys
 sys.path.append('/usr/lib/freecad/lib/')
 from breeder_blanket_model_maker.DCLL_CAD_procedure import *
 from breeder_blanket_model_maker.HCLL_CAD_procedure import *
-from breeder_blanket_model_maker.HCLL_CAD_procedure_remi import *
 from breeder_blanket_model_maker.HCPB_CAD_procedure import *
 from breeder_blanket_model_maker.WCLL_CAD_procedure import *
 
@@ -36,9 +35,6 @@ def detailed_module(dict_or_list_of_blanket_geometry_parameters):
 
     if blanket_geometry_parameters_dict['blanket_type'].upper() == 'HCLL':
        detailed_modules_parts = p.map(HCLL_detailed_module,blanket_geometry_parameters_list) 
-
-    elif blanket_geometry_parameters_dict['blanket_type'].upper() == 'HCLL_REMI':
-       detailed_modules_parts = p.map(HCLL_detailed_module_remi,blanket_geometry_parameters_list) 
 
     elif blanket_geometry_parameters_dict['blanket_type'].upper() == 'DCLL':
        detailed_modules_parts = p.map(DCLL_detailed_module, blanket_geometry_parameters_list) 
